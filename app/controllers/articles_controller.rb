79302f8fe6c72767da7b9ad84b18434fd2c82a03
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-11 06:45:28 +0900

    Article #edit,update,delete,confirm実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,39 +1,57 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only:[:show,:edit,:update,:destroy,]
+
   def index
     @articles = Article.all
   end
 
   def new
-    @article = Article.new
+    if params[:back]
+      @article = Article.new(article_params)
+    else
+      @article = Article.new
+    end
   end
 
   def confirm
+    @article = Article.new(article_params)
+    render :new if @article.invalid?
   end
 
   def create
     @article = Article.create(article_params)
     if @article.save
-      redirect_to "/", notice: "記事を投稿しました"
+      redirect_to articles_path, notice: "記事を投稿しました"
     else
       render 'new'
     end
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to article_path, notice: "記事を編集しました"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @article.destroy
+    redirect_to articles_path, notice: "記事を削除しました"
   end
 
   private
   def article_params
     params.require(:article).permit(:title,:content,:genre_id)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
