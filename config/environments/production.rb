3f2ee336df9a3d7f796a76a71377ffee1e786e0b
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-11 16:12:35 +0900

    Sendgrid用の設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,4 +22,15 @@ Rails.application.configure do
   end
 
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'https://hidden-plains-48137.herokuapp.com/' }
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: "heroku.com",
+    address: "smtp.SendGrid.net",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
