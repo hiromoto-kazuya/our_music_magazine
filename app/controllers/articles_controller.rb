5e2a10f8ae531c5a82583725db193eb3a2aad8cb
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-24 17:56:43 +0900

    PV数の表示を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,7 @@ class ArticlesController < ApplicationController
   before_action :set_params_for_searching_articles_and_users, only: [:index, :new, :confirm, :edit, :show, :hashtag]
 
   def index
+
   end
 
   def new
