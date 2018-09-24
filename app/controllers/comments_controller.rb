0247176c27e63652038b66f48a969e9fcde6cc59
廣本 和也 <kazuya@kazuya-macbook.local>
2018-09-24 16:59:53 +0900

    articles#showページ、comment機能にuser情報紐付け

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,17 +2,18 @@ class CommentsController < ApplicationController
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.build(comment_params)
+    @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
         format.js { render :index }
       else
-        format.html { redirect_to blog_path(@article), notice: '投稿に失敗しました' }
+        format.html { redirect_to article_path(@article), notice: '投稿に失敗しました' }
       end
     end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:article_id, :content)
+    params.require(:comment).permit(:article_id,:user_id, :content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_19_050923) do
+ActiveRecord::Schema.define(version: 2018_09_24_073356) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_09_19_050923) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
