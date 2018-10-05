e82faf0729b1459961bb88154a50a2c08a6b5acc
廣本 和也 <kazuya@kazuya-macbook.local>
2018-10-05 20:08:54 +0900

    投稿フォームのデザイン調整

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ApplicationRecord
-  validates :title,   presence: true, length: { maximum: 50 }
+  validates :title,   presence: true, length: { maximum: 100 }
   validates :content, presence: true
 
   belongs_to :user
